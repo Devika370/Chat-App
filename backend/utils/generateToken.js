80429fe205bf7024d703fcf4edd6d8802d2ace67
Devika370 <https://github.com/Devika370>
2025-07-29 13:24:13 +0400

    Modify generatetoken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ res.cookie("jwt" , token,{
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly:true,
     sameSite:"strict",
-    secure:process.env.NODE_ENV != "developement"
+    secure:process.env.NODE_ENV != "development"
 
 })
 
