0ca1f4eb523c948dcd63a951ca4644ab74dbcf35
Devika370 <https://github.com/Devika370>
2025-07-30 17:00:59 +0400

    update useGetMessages.js file and .env file

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,13 +5,14 @@ const token = jwt.sign({userId}, process.env.JWT_SECRET,{
 
     expiresIn:'15d'
 });
-res.clearCookie("jwt");
+
 res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
 		sameSite: "None", // CSRF attacks cross-site request forgery attacks
 		secure: process.env.NODE_ENV !== "development",
 	});
+ 
 
 }
 
