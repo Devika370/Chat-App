ecedc3b9a760c48d20b1679be830b97d4edad7da
Devika370 <https://github.com/Devika370>
2025-07-30 14:31:22 +0400

    update backend generateToken file

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,13 +5,12 @@ const token = jwt.sign({userId}, process.env.JWT_SECRET,{
 
     expiresIn:'15d'
 })
-res.cookie("jwt" , token,{
-    maxAge: 15 * 24 * 60 * 60 * 1000,
-    httpOnly:true,
-    sameSite: "None",
-    secure:true
-
-})
+res.cookie("jwt", token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		secure: process.env.NODE_ENV !== "development",
+	});
 
 }
 
