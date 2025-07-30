b51024b0b109cb182665bcd8a22d0c148a113e0b
Devika370 <https://github.com/Devika370>
2025-07-30 12:45:16 +0400

    update backend socket file and frontend socketContext file

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server , {
     cors: {
-        origin: ['http://localhost:3000'],
+        origin: ['https://chat-app-real-frontend.onrender.com'],
         methods: ['GET', 'POST'],
         credentials:true,
     }
